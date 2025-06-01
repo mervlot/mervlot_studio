3d9dc0a41662e660a9243a916b9a037fd501d92e
Muhammad <princeadeite09@gmail.com>
2025-06-01 23:52:02 +0100

    Updates

---
+++ assets/Navbar-DlBb6y49.js
@@ -0,0 +1,21 @@
+import{j as r}from"./index-vKWjDMaW.js";/* empty css              */import{m as n}from"./proxy-08qRjFmM.js";function o(){return r.jsx(n.nav,{className:`\r
+  \r
+   \r
+   justify-center\r
+    justify-items-center\r
+    w-[90%]\r
+    bg-white/20\r
+    dark:bg-black/30 \r
+    backdrop-blur-md \r
+    border\r
+    border-white/10\r
+    rounded-xl\r
+    px-4\r
+    py-2\r
+\r
+   justify-self-center\r
+    \r
+   \r
+    shadow-lg\r
+    z-50\r
+    `,initial:{opacity:0,y:-10},animate:{opacity:1,y:1},transition:{duration:1},children:r.jsx("h2",{className:"logo-m text-[#2e89ec] ",children:"Mervlot Studio"})})}export{o as default};
